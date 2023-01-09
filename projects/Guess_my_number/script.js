10abfdd6ea86d329d003547001fe5f7d0ed5ffae
Wilmer <wilmerpolanco13@gmail.com>
2023-01-08 23:10:28 -0500

    Completed the game logic, added restart action when clicking on again button!

--- projects/Guess_my_number/script.js
+++ projects/Guess_my_number/script.js
@@ -1,19 +1,26 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) +1;
-document.querySelector('.number').textContent = secretNumber;
-
-let score = 20;
+   let secretNumber = Math.trunc(Math.random() * 20) +1;
+   console.log(secretNumber);
+   let score = 20;
 
 document.querySelector('.check').addEventListener('click', function (){
    const guess = Number(document.querySelector('.guess').value);
    
+   //When there is no input
    if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
    }
+
+   //When player wins
    else if (guess === secretNumber){
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
    }
+
+   //When guess is too high
    else if (guess > secretNumber) {
       if (score > 1) {
          document.querySelector('.message').textContent = '📈Too high';
@@ -21,11 +28,14 @@ document.querySelector('.check').addEventListener('click', function (){
          document.querySelector('.score').textContent = score;
       }
       else {
+         // when player loses the game due to high guess
          document.querySelector('.message').textContent = '💥You lost the game!';
          document.querySelector('.score').textContent = 0;
       }
    
    }
+
+   //when player guess is too low
    else if (guess < secretNumber) {
 
           if (score > 1) {
@@ -33,13 +43,29 @@ document.querySelector('.check').addEventListener('click', function (){
                score--;
                document.querySelector('.score').textContent = score;
       }
+         //when player loses due to low guess
           else {
               document.querySelector('.message').textContent = '💥You lost the game!';
               document.querySelector('.score').textContent = 0;
          }
   
       }
+      document.querySelector('.again').addEventListener('click', function(){
+         secretNumber = Math.trunc(Math.random() * 20) +1;
+         score=20;
+         document.querySelector('.message').textContent = 'Start guessing...';
+         document.querySelector('.number').textContent = '?';
+         document.querySelector('.number').style.width = '15rem';
+         document.querySelector('.score').textContent = score;
+         document.querySelector('body').style.backgroundColor = '#222';
+         document.querySelector('.guess').value = '';
 
-});
+      });
+
+   });
+
+
+
+
 
     
