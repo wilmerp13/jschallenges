bc985e438cd050d5d0aeb4c8471303daedcbfe55
Wilmer <wilmerpolanco13@gmail.com>
2023-01-08 19:15:41 -0500

    added score variable to count failures and added scenarios

--- projects/Guess_my_number/script.js
+++ projects/Guess_my_number/script.js
@@ -4,6 +4,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) +1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function (){
    const guess = Number(document.querySelector('.guess').value);
    
@@ -15,8 +17,15 @@ document.querySelector('.check').addEventListener('click', function (){
    }
    else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈Too high';
+    score--;
+    document.querySelector('score').textContent = score;
    }
-
+   else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '📉Too low';
+      score--;
+      document.querySelector('score').textContent = score;
+     }
+  
 
 });
 
